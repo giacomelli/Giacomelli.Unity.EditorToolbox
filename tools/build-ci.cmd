87ab6ad0317ba490de3a591be3f91698007b6166
Diego Giacomelli <giacomelli@gmail.com>
2016-08-13 18:48:03 -0300

    Testing appveyor.

--- tools/build-ci.cmd
+++ tools/build-ci.cmd
@@ -1,12 +1,12 @@
 echo Building Giacomelli.Unity.EditorToolbox.dll...
 
-mkdir C:\projects\Giacomelli.Unity.EditorToolbox\build
-cd C:\projects\Giacomelli.Unity.EditorToolbox\src\Giacomelli.Unity.EditorToolbox\bin\Release
+mkdir C:\projects\Giacomelli-Unity-EditorToolbox\build
+cd C:\projects\Giacomelli-Unity-EditorToolbox\src\Giacomelli.Unity.EditorToolbox\bin\Release
 
 echo Calling ILRepack...
-C:\projects\Giacomelli.Unity.EditorToolbox\tools\ILRepack.exe /target:library /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:C:\projects\Giacomelli.Unity.EditorToolbox\build\Giacomelli.Unity.EditorToolbox.dll /wildcards Giacomelli.Unity.*.dll
+C:\projects\Giacomelli-Unity-EditorToolbox\tools\ILRepack.exe /target:library /out:C:\projects\Giacomelli-Unity-EditorToolbox\build\Giacomelli.Unity.EditorToolbox.dll /wildcards Giacomelli.Unity.*.dll
 echo ILRepack finished.
 
-cd C:\projects\Giacomelli.Unity.EditorToolbox
+cd C:\projects\Giacomelli-Unity-EditorToolbox
 
 echo done!
