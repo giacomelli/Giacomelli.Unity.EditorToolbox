5e68595e180b5d656f499349a7ca6fcff208aca6
Diego Giacomelli <giacomelli@gmail.com>
2016-10-29 18:53:31 -0200

    Using mono registered.

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,6 @@ mkdir ../build
 
 cd ../src/Giacomelli.Unity.EditorToolbox/bin/Release
 
-/Library/Frameworks/Mono.framework/Versions/4.4.2/bin/mono ../../../../tools/ILRepack.exe /target:library /out:../../../../build/Giacomelli.Unity.EditorToolbox.dll /wildcards Giacomelli.Unity.*.dll
+mono ../../../../tools/ILRepack.exe /target:library /out:../../../../build/Giacomelli.Unity.EditorToolbox.dll /wildcards Giacomelli.Unity.*.dll
 
 cd ../../../../tools
-
