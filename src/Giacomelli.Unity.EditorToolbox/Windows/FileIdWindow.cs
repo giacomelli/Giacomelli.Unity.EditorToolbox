3328ffc80188bd3f4719fee7c1a54e898d68ed34
Diego Giacomelli <giacomelli@gmail.com>
2016-10-29 18:52:53 -0200

    Added debug window to generate FileId based on Type name.

---
+++ src/Giacomelli.Unity.EditorToolbox/Windows/FileIdWindow.cs
@@ -0,0 +1,41 @@
+﻿#if DEBUG
+using Giacomelli.Unity.Metadata.Infrastructure.Framework.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+namespace Giacomelli.Unity.EditorToolbox
+{
+	public class FileIdWindow : WindowBase
+    {
+        #region Fields
+      	private string m_typeName = string.Empty;
+		#endregion
+
+        #region Constructors
+        public FileIdWindow()
+            : base("FileId", 400, 400)
+        {
+        }
+        #endregion
+
+        #region GUI
+        [MenuItem("Giacomelli/FileId", priority = 2)]
+        private static void Init()
+        {
+            var instance = GetWindow<FileIdWindow>();
+            instance.ShowUtility();
+        }
+
+        protected override void PerformOnGUI()
+        {
+			m_typeName = GUILayout.TextField(m_typeName);
+
+			if (!string.IsNullOrEmpty(m_typeName))
+			{
+				GUILayout.Label(FileIdUtil.FromType(m_typeName).ToString());
+			}
+        }
+        #endregion
+    }
+}
+#endif
